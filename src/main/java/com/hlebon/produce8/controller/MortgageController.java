2e6a23743456762aca26e111088c38687c434ace
Hleb Bandarenka <hleb.bandarenka@gmail.com>
2023-09-16 12:39:01 -0700

    added responses in swagger

--- src/main/java/com/hlebon/produce8/controller/MortgageController.java
+++ src/main/java/com/hlebon/produce8/controller/MortgageController.java
@@ -1,9 +1,15 @@
 package com.hlebon.produce8.controller;
 
+import com.hlebon.produce8.controller.dto.ErrorDto;
 import com.hlebon.produce8.controller.dto.MortgageRequestDto;
+import com.hlebon.produce8.controller.dto.MortgageResponseDto;
 import com.hlebon.produce8.mapper.MortgageMapper;
 import com.hlebon.produce8.service.MortgageService;
 import com.hlebon.produce8.service.response.MortgageResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +38,23 @@ public class MortgageController {
         this.mortgageService = mortgageService;
     }
 
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                            schema = @Schema(implementation = MortgageResponseDto.class))
+                            }),
+                    @ApiResponse(
+                            responseCode = "400",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                            schema = @Schema(implementation = ErrorDto.class))
+                            })
+            })
     @PostMapping("/calculate")
     public ResponseEntity<?> calculate(
             @RequestBody MortgageRequestDto mortgageRequest
